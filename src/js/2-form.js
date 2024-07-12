f973c1d9bb76faf0f241506f28f877746a6fa957
alena_sinitsa <alenasinitsa85@gmail.com>
2024-07-12 21:39:37 +0300

    form2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,24 +1,29 @@
-const form = document.querySelector(`form.feedback-form`);
-console.dir(form);
-
 const formData = { email: '', message: '' };
-console.dir(formData);
+
+const form = document.querySelector('form.feedback-form');
+
+populateInput();
 
 form.addEventListener('input', handleInput);
 
 function handleInput(event) {
-  event.preventDefault();
+  if (event.target.matches('[name="email"]')) {
+    formData.email = event.target.value;
+  } else if (event.target.matches('[name="message"]')) {
+    formData.message = event.target.value;
+  }
 
-  let inputEmail = form.elements.email.value.trim();
-  let inputMessage = form.elements.message.value.trim();
-
-  console.log(inputEmail);
-  console.log(inputMessage);
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-// let formObj = {
-//   email: inputEmail,
-//   password: inputPassword,
-// };
+function populateInput() {
+  const savedInput = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-form.reset();
+  if (savedInput) {
+    formData.email = savedInput.email;
+    formData.message = savedInput.message;
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+  console.log(savedInput);
+}
